d310c17879bc69c786a3f8ca1a869de62ebe13ba
User <breakathi@gmail.com>
2025-09-11 17:43:11 +0200

    Added screenshort on Hook class

--- src/test/java/StepDef/Hooks.java
+++ src/test/java/StepDef/Hooks.java
@@ -1,4 +1,22 @@
 package StepDef;
 
+import Utilities.AppiumDriverFactory;
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+
 public class Hooks {
+
+    protected static AndroidDriver driver;
+
+    public Hooks(){
+        driver = AppiumDriverFactory.getDriver();
+    }
+
+    public void takeScreenshotOnFailure(Scenario scenario){
+        if(scenario.isFailed()){
+            byte[] screenshot = driver.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "screenshot");
+        }
+    }
 }
